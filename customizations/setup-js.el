41d3ae604134feaef11d4f283fe662a3e07769c4
Mikko Hämäläinen <mikko.hamalainen@greyarealabs.com>
2015-03-01 17:35:19 +0200

    js2-mode and ac-js2/autocomplete

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,9 +1,13 @@
 ;; javascript / html
 ;(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (add-to-list 'auto-mode-alist '("\\.js$\\'" . js2-mode))
+
 (add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'html-mode-hook 'subword-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq js-indent-level 2)
+(setq js2-highlight-level 3)
+
 (eval-after-load "sgml-mode"
   '(progn
      (require 'tagedit)
